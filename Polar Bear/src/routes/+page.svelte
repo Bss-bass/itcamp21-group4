3ec9161162efafcefb83dc91303455bbea45375e
Siwapat Samantrakulchai <109400767+Bss-bass@users.noreply.github.com>
2025-04-30 23:33:22 +0700

    Update +page.svelte

--- Polar Bear/src/routes/+page.svelte
+++ Polar Bear/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="bg-black w-screen h-screen flex relative justify-center">
     <img id="scene2 -z-2" src={'/landing.png'} alt="" />
-    <div class="absolute flex flex-col gap-8 items-center justify-center right-[50] bottom-[30%]">
+    <div class="absolute flex flex-col gap-8 mx-auto bottom-[30%]">
         <h1 class="text-center text-7xl text-[#040348]">BEAR POLAR</h1>
         <button class="bg-[#3e5879] text-white cursor-pointer px-9 py-3 rounded-full" onclick={() => {goto('story')}}>เริ่มต้น</button>
     </div>
